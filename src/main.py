b4570bd61c2494e3dc5a920a9c00fec93c5ac859
dfjds <arielsalama722@gmail.com>
2026-07-14 18:36:23 -0400

    Initialized python project and created basic input system

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Enter the number of seconds you want to run the test for:", end=" ")
+    seconds = int(input())
+    
+    print(">", end=" ")
+    typed = input()
+
+main()
